857cdf5d07c100333e7ed11fbc99e7e8ffa9ecbf
yuriaguiar-86 <kakacraft337@gmail.com>
2022-09-26 13:53:53 -0300

    subindo app final

--- src/app/pokelist/pokelist.component.ts
+++ src/app/pokelist/pokelist.component.ts
@@ -1,5 +1,5 @@
 import { PokemonService } from '../pokemon.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pokelist',
@@ -12,20 +12,31 @@ export class PokelistComponent implements OnInit {
   snorlax = '/assets/snorlax.png';
   pokeball = '/assets/pokeball.png';
 
-  pokemons: any;
+  scanner: string;
   error: boolean = false;
+
+  public pokemons: any;
+  private pokemonsFilters: any;
 
   constructor( private service: PokemonService ) { }
 
   ngOnInit(): void {
     this.service.index()
       .subscribe(res => {
-        console.log(res.results)
-        this.pokemons = res.results
+        this.pokemonsFilters = res.results;
+        this.pokemons = this.pokemonsFilters;
       },
       errors => {
         this.error = true
       }
     );
+  }
+
+  public searchPokemon(value: string) {
+    const filter = this.pokemonsFilters.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.pokemons = filter;
   }
 }
